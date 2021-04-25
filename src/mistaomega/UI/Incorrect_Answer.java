28451d153cd5c2dc88bcb85254ce5fb3866fd1e7
Declan <24239089@edgehill.ac.uk>
2021-04-25 18:46:19 +0100

    kill me daddy

---
+++ src/mistaomega/UI/Incorrect_Answer.java
@@ -0,0 +1,25 @@
+package mistaomega.UI;
+
+import javax.swing.*;
+
+public class Incorrect_Answer {
+    private JPanel Correct;
+    private JPanel mainPanel;
+    private JButton btnQuestion1;
+    private JButton btnQuestion2;
+    private JButton btnQuestion3;
+    private JButton btnQuestion4;
+    private JPanel imagePan;
+    private JLabel lblQuestionTitle;
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here"""
+
+        JFrame frame = new JFrame();
+        ImageIcon icon = new ImageIcon("D:\\April Courseworks\spacepic.jpg");
+        JLabel label = new JLabel(icon);
+
+    }
+}
+
